cf7da516c0d4791a97a4f9bf647c696086b53286
Divyanshu Padhy <divyadollypadhy@outlook.com>
2021-12-17 08:32:01 -0800

    Initial Setup

---
+++ main.py
@@ -0,0 +1,14 @@
+import pyautogui as ptg # for automating the mouse and keyboard
+from time import sleep
+
+from utils import * # Utilities
+
+with open("./creds/zoom_location.txt") as location:
+    location = location.read()
+
+
+if __name__ == "__main__":
+    """
+    TODO: Add a function to open a zoom, click and type the required buttons and text and finally join the meeting at the meeting_time
+    """
+    pass
---
+++ utils/ZoomMeeting.py
@@ -0,0 +1,5 @@
+class ZoomMeeting:
+    def __init__(self, meeting_id, passcode, meeting_time) -> None:
+        self.id = meeting_id
+        self.password = passcode
+        self.time = meeting_time
